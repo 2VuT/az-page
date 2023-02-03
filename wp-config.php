3c9ead64d15d296da02f8f92af29851caa0fae9b
HaiPTIT <haivtndc@gmail.com>
2023-02-04 04:14:56 +0700

    update image link

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'az_page' );
+define( 'DB_NAME', 'az_soft' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'azsoft' );
 
 /** Database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'admin1234' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
